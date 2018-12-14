af4b3860905ae0ef4b6932e758a62a12badc3f48
Doug Enas <douglassenas@gmail.com>
2018-12-14 13:06:21 -0500

    a

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require("dotenv-safe").config({ allowEmptyValues: true });
+require('dotenv').config({silent: true})
 const express = require("express");
 const morgan = require("morgan");
 const passport = require("passport");
